681c059d56312da68281984cc3eff4ed7062cc2b
Unknown <filipe.lenoirmaiaesilva@viacesi.fr>
2018-04-17 14:16:20 +0200

    add boite_id
    script qui permet d'ajouter un idee a la boite à idee

---
+++ Site/boite_id.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <form method="post" action="boite_id.php">
+    <p>
+        Entrez un titre : </br>
+        <input type="text" name="name" /> </br></br> <!--ajout du titre-->
+        Entrez votre description : </br>
+        <textarea name="description" row="5" cols="70"></textarea> </br></br><!--ajout description!-->
+        <input type="submit" name="envoie" value="Ajouter l'évènement" /><!--envoie des donnees-->
+    </p>
+
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=bde;charset=utf8', 'root', '');
+
+  if(isset($_POST['envoie']))
+  {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $id_user = $_SESSION['id_user'];		//ajout de l'id user connecté à la session actuelle 
+
+    $req = $bdd->prepare('INSERT INTO suggestion_event(name, description, id_user) VALUES(?, ?, ?)'); //enregistrement dans la bdd
+    $req->execute(array($name, $description, $id_user));
+  }
+?>
+</body>
+</html>
