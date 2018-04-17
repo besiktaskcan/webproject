240b089d4530134fb6334186070ba037e14fcf52
Neotarlax <benjamin.raunet@gmail.com>
2018-04-17 09:23:04 +0200

    Changement boutique.php
    Modification du template pour que ça fonctionne

--- Site/boutique.php
+++ Site/boutique.php
@@ -8,7 +8,7 @@ session_start();
     <head>
         <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
         <meta http-equiv="refresh" content=""/>
-        <meta name="author" content="Florian Pfeifer">
+        <meta name="author" content="Benjamin RAUNET">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Cantarell?<?php echo time(); ?>" />
         <link rel="stylesheet" media="(orientation:portrait)" href="css/style2.css?<?php echo time(); ?>">
         <link rel="stylesheet" media="(orientation:landscape)" href="css/style.css?<?php echo time(); ?>">
@@ -19,7 +19,7 @@ session_start();
         <title>BDE EXIA CESI ST</title>
 
         <script>
-        function showUser(str) {
+        function showGoodies(str) {
           if (str=="") {
             document.getElementById("txtHint").innerHTML="";
             return;
@@ -35,7 +35,7 @@ session_start();
               document.getElementById("txtHint").innerHTML=this.responseText;
             }
           }
-          xmlhttp.open("GET","getimage.php?q="+str,true);
+          xmlhttp.open("GET","getimageBoutique.php?q="+str,true);
           xmlhttp.send();
         }
         </script>
@@ -45,13 +45,15 @@ session_start();
 
 
 
-    <body onload="showUser(this.value)">
+    <body onload="showGoodies(this.value)">
 
       <header>
         <?php include("menuBar.php"); ?>
       </header>
 
-
+	<section id="s1_boutique">      
+		<div id="txtHint"><b></b></div>
+	</section>
 
 
 
